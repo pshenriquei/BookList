93492a605deb78b22f1b6ea4107d713c8da01e7c
Pedro Henrique <82547663+pshenriquei@users.noreply.github.com>
2023-06-18 17:33:41 -0300

    Update BookInfoActivity.kt

--- app/src/main/java/com/example/bookinglist/presenter/activities/BookInfoActivity.kt
+++ app/src/main/java/com/example/bookinglist/presenter/activities/BookInfoActivity.kt
@@ -4,14 +4,21 @@ package com.example.bookinglist.presenter.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ProgressBar
 import android.widget.Toast
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.ActionBar
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 import com.example.bookinglist.MainActivity
 import com.example.bookinglist.R
 import com.example.bookinglist.data.infra.ListOfBooksApi
 import com.example.bookinglist.data.infra.retroFit
 import com.example.bookinglist.data.model.ListBookModel
 import com.example.bookinglist.databinding.ActivityBookInfoBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -19,56 +26,83 @@ import retrofit2.Response
 class BookInfoActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBookInfoBinding
+    private lateinit var progressBar: ProgressBar
+    private lateinit var btnBack: ImageButton
+    private var isBackButtonEnabled = true
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBookInfoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportActionBar!!.hide()
-        Toolbar()
+        progressBar = findViewById(R.id.progressBar2)
+        progressBar.visibility = View.GONE
 
-        val id: String = intent.getStringExtra("id").toString()
+        loadData()
 
-        retroFit().create(ListOfBooksApi::class.java)
-            .getBooksById(id)
-            .enqueue(object : Callback<ListBookModel> {
-                override fun onFailure(call: Call<ListBookModel>, t: Throwable) {
-                    Toast.makeText(applicationContext, "Server Error!", Toast.LENGTH_SHORT).show()
+
+        supportActionBar?.apply {
+            displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+            setCustomView(R.layout.activity_book_info)
+        }
+        btnBack = supportActionBar!!.customView.findViewById(R.id.btnBack)
+        btnBack.setOnClickListener {
+            if (isBackButtonEnabled) {
+                progressBar.isVisible = true
+                isBackButtonEnabled = false
+                btnBack.isEnabled = false
+
+                lifecycleScope.launch {
+                    val intent = Intent(this@BookInfoActivity, MainActivity::class.java)
+                    delay(2000)
+                    startActivity(intent)
+                    finish()
                 }
-
-                override fun onResponse(
-                    call: Call<ListBookModel>,
-                    response: Response<ListBookModel>
-                ) {
-                    if (response.isSuccessful) {
-                        response.body()?.let {
-                            binding.titleBookText.text = "Title: ${it.results.first().title}"
-                            binding.authorNameText.text = "Author: ${it.results.first().authors.first().name}"
-                            binding.authorBirthText.text = "Author's birth year: ${it.results.first().authors.first().birth_year}"
-                            binding.authorDeathText.text = "Author's death year: ${it.results.first().authors.first().death_year}"
-                            binding.subject.text = "Subject: ${it.results.first().subjects.first()}"
-
-                        }
-                    }
-                }
-
             }
-            )
-    }
-
-    private fun Toolbar(){
-        val toolbarBookInfo = binding.toolbarBookInfo
-
-        val backIcon = getDrawable(R.drawable.ic_back)
-        backIcon?.setTint(ContextCompat.getColor(this, R.color.black))
-        toolbarBookInfo.navigationIcon = backIcon
-        toolbarBookInfo.setNavigationOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-
         }
     }
 
+override fun onResume() {
+    super.onResume()
+    isBackButtonEnabled = true
+    btnBack.isEnabled = true
+    progressBar.visibility = View.VISIBLE
+    lifecycleScope.launch {
+        delay(2000)
+        progressBar.visibility = View.GONE
+    }
+}
+
+private fun loadData() {
+    val id: String = intent.getStringExtra("id").toString()
+
+    retroFit().create(ListOfBooksApi::class.java)
+        .getBooksById(id)
+        .enqueue(object : Callback<ListBookModel> {
+            override fun onFailure(call: Call<ListBookModel>, t: Throwable) {
+                Toast.makeText(applicationContext, "Server Error!", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onResponse(
+                call: Call<ListBookModel>,
+                response: Response<ListBookModel>
+            ) {
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        binding.titleBookText.text = "Title: ${it.results.first().title}"
+                        binding.authorNameText.text =
+                            "Author: ${it.results.first().authors.first().name}"
+                        binding.authorBirthText.text =
+                            "Author's birth year: ${it.results.first().authors.first().birth_year}"
+                        binding.authorDeathText.text =
+                            "Author's death year: ${it.results.first().authors.first().death_year}"
+                        binding.subject.text = "Subject: ${it.results.first().subjects.first()}"
+
+                    }
+                }
+            }
+        }
+        )
+}
 }
